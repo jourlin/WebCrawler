521a28cc74c5982a48294e1a4560f237b0688ff1
Jourlin <jourlin@jourlin-VirtualBox.(none)>
2011-09-23 17:21:44 +0200

    Some more work on splitting URL informations

--- sql/CreateTables.sql
+++ sql/CreateTables.sql
@@ -1,10 +1,24 @@
+DROP TABLE IF EXISTS domain CASCADE;
+CREATE TABLE domain (
+    id BIGSERIAL PRIMARY KEY,
+    checked timestamp with time zone,
+    protocol VARCHAR(50) NOT NULL,
+    subdomains VARCHAR(256),
+    domain VARCHAR(256) NOT NULL,
+    tld VARCHAR(10) NOT NULL,
+    ipadr inet,
+    UNIQUE(protocol, subdomains, domain, tld)
+);
+
 DROP TABLE IF EXISTS node CASCADE;
 CREATE TABLE node (
     id BIGSERIAL PRIMARY KEY,
     url text NOT NULL UNIQUE,
     effectiveurl text,
     checked timestamp with time zone,
-    tld VARCHAR(10),
+    score double,
+    domainid bigint NOT NULL REFERENCES domain(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    urlpath text,
     content text
 );
 
@@ -13,15 +27,13 @@ CREATE TABLE links (
     id BIGSERIAL PRIMARY KEY,
     "from" bigint NOT NULL REFERENCES node(id) ON UPDATE CASCADE ON DELETE CASCADE,
     "to" bigint NOT NULL REFERENCES node(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    leftcontext text,
+    midcontext text,
+    rightcontext text,
     UNIQUE ("from", "to"),
     checked timestamp with time zone
 );
 
-DROP TABLE IF EXISTS domain CASCADE;
-CREATE TABLE domain (
-    id BIGSERIAL PRIMARY KEY,
-    name text NOT NULL UNIQUE
-);
 
 CREATE TEXT SEARCH CONFIGURATION public.pg ( COPY = pg_catalog.simple); 
 CREATE TEXT SEARCH DICTIONARY scratch (TEMPLATE = simple);
