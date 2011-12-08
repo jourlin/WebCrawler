0792a23f49290d03f132ef9b39f18ba1a6553017
Pierre Jourlin <jourlin@jourlin-VirtualBox.(none)>
2011-12-08 11:57:11 +0100

    Version with URL user-defined type seems to work !

--- sql/CreateTables.sql
+++ sql/CreateTables.sql
@@ -25,37 +25,43 @@ CREATE TABLE links (
 );
 
 DROP VIEW IF EXISTS linksview;
-CREATE VIEW linksview AS SELECT links.from as from, url as to, midcontext FROM links, nodeview WHERE links.to = nodeview.id;
+CREATE VIEW linksview AS SELECT links.from as from, url as to, midcontext FROM links, node WHERE links.to = node.id;
 
 CREATE OR REPLACE FUNCTION linksview_insert_row() RETURNS TRIGGER AS $linksview$
 DECLARE
 to_id BIGINT;
+link_id BIGINT;
 BEGIN
-SELECT INTO to_id "id" FROM "node" WHERE url_out("url")=NEW."to";
+SELECT INTO to_id "id" FROM "node" WHERE "url"=NEW."to";
         IF to_id IS NULL THEN
-         INSERT INTO node (url) VALUES (url_in(NEW."to")) INTO to_id RETURNING id;
+         INSERT INTO node (url, score) VALUES (NEW."to", ScoreURL(NEW."to")) INTO to_id RETURNING id;
         END IF;
-INSERT INTO links ("from", "to", midcontext, checked) VALUES (NEW."from", to_id, NEW.midcontext, now());
+SELECT INTO link_id "id" FROM "links" WHERE "from"=NEW."from" AND "to"=to_id;
+IF link_id IS NULL THEN
+    INSERT INTO links ("from", "to", midcontext, checked) VALUES (NEW."from", to_id, NEW.midcontext, now());
+END IF;
 RETURN NEW;
         RETURN NULL; -- result is ignored since this is an AFTER trigger
 END;
+
+
 $linksview$ LANGUAGE plpgsql;
 
-DROP FUNCTION IF EXISTS ScoreURL(nodeid bigint);
-CREATE FUNCTION ScoreURL(nodeid bigint) RETURNS bigint AS 
+CREATE TRIGGER linksview_insert
+    INSTEAD OF INSERT ON linksview
+    FOR EACH ROW
+    EXECUTE PROCEDURE linksview_insert_row();
+
+DROP FUNCTION IF EXISTS ScoreURL(url url);
+CREATE FUNCTION ScoreURL(url url) RETURNS bigint AS 
 $$
 DECLARE
-score bigint;
-tmp bigint;
 BEGIN
-select (CASE WHEN (substring(lower(url_out("url")), 'presidentielle') IS NULL) THEN 0 ELSE 2 END)+
-(CASE WHEN (substring(lower(url_out("url")), 'politique') IS NULL) THEN 0 ELSE 1 END)+
-(CASE WHEN (substring(lower(url_out("url")), 'election') IS NULL) THEN 0 ELSE 1 END)+
-(CASE WHEN (substring(lower(url_out("url")), 'primaires') IS NULL) THEN 0 ELSE 3 END)+
-(CASE WHEN url_top("url")='fr' THEN 10 ELSE 0 END)+
-(CASE WHEN (substring(lower(url_out("url")), 'socialistes') IS NULL) THEN 0 ELSE 3 END)
-INTO score FROM node where node."id"=nodeid;
-RETURN score;
+IF CAST(url_top(url) AS TEXT) ='fr' THEN
+	RETURN 1;
+ELSE
+	RETURN 0;
+END IF;
 END;
 $$ LANGUAGE plpgsql;
 
